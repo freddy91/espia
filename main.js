8ce98913f17be33cccb83c269cb24beef6753d7f
freddy91 <49192031+freddy91@users.noreply.github.com>
2020-07-17 23:37:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //let locations = ["Playa", "Banco", "Hotel", "Rodaje de una película", "Teatro", "Sierra Nevada", "Hospital", "Base militar", "Embajada", "Zoológico", "Estación espacial", "Crucero", "Avión", "Circo", "Comisaría de policía", "Supermercado", "Universidad", "Parque de atracciones", "Carnaval", "Discoteca", "Fiesta de empresa", "Casino", "Restaurante", "Colegio", "Spa", "Batalla campal", "Tren de pasajeros", "Barco pirata", "Submarino", "Gasolinera"];
 let spyString = "Eres el espía";
 
-let locations = ["Springfield","Doney", "Casoplon del coletas","Salamanca","Bernabéu","Ikea","Cementerio","Estanco","Muralla","La Cueva","Elefante de Oro,"Mirandor de Montepinar,"Corazón de Pani","La Marina","Tarragona","Valorio","Polígono de la Hiniesta","Benidorm","Azerbaiyán","Eurovisión","Maquina del tiempo","Sanabria","El Claudio"];
+let locations = ["Springfield","Doney", "Casoplon del coletas","Salamanca","Bernabéu","Ikea","Cementerio","Estanco","Muralla","La Cueva","Elefante de Oro,"Mirandor de Montepinar","Corazón de Pani","La Marina","Tarragona","Valorio","Polígono de la Hiniesta","Benidorm","Azerbaiyán","Eurovisión","Maquina del tiempo","Sanabria","El Claudio","Cara oculta de la luna","Tienda de zapatos"];
 
 let currentLocation = document.getElementById("currentLocation");
 
