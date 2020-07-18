68aa64fd4113470d848b76cf2a514c7e4f0deb37
freddy91 <49192031+freddy91@users.noreply.github.com>
2020-07-18 13:25:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function revealLocation(event) {
 	if (spy == form.player.value)
 		currentLocation.innerHTML = spyString;
 	else
-		currentLocation.innerHTML = myChance.pickone(locations);
+		currentLocation.innerHTML = spy;
 }
 
 let seed = document.getElementById("seed");
@@ -44,5 +44,5 @@ function selectInput(input) {
 }
 
 
-
+//currentLocation.innerHTML = myChance.pickone(locations);
 
